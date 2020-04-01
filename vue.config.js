98ec6288ce4951dc699d34b43f12bfb7ee8b970f
SolAZDev <SolAZDev@gmail.com>
2020-03-31 22:55:17 -0400

    Attempt 4.5

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@ module.exports = {
   pwa: {
     name: "DreamReader"
   },
+
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].title = "DreamReader - Dream Interpretations";
       return args;
     });
   },
-  publicPath: "/DreamReader/",
-  assetsDir: "assets"
+
+  publicPath: '/DreamReader/',
+  assetsDir: 'assets',
+  outputDir: 'docs'
 };
