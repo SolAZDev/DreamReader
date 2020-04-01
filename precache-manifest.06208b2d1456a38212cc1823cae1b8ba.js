6fbbc9510bc8f2f6bfee503fa5ebfa714d11c4e8
SolAZDev <SolAZDev@gmail.com>
2020-04-01 09:42:33 -0400

    Version 1.2b - Small Context Fix

---
+++ precache-manifest.06208b2d1456a38212cc1823cae1b8ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81a720078d3f2cd2c62762dcdc916b3",
+    "url": "/DreamReader/Symbols.json"
+  },
+  {
+    "revision": "a7ac80b4cea24041201c",
+    "url": "/DreamReader/assets/css/about.da192eb7.css"
+  },
+  {
+    "revision": "a10edbdb70771f9002af",
+    "url": "/DreamReader/assets/css/app.26acaa1c.css"
+  },
+  {
+    "revision": "95db0d3babf9be977ff1",
+    "url": "/DreamReader/assets/css/chunk-vendors.4304d73c.css"
+  },
+  {
+    "revision": "a7ac80b4cea24041201c",
+    "url": "/DreamReader/assets/js/about.02bf3a96.js"
+  },
+  {
+    "revision": "a10edbdb70771f9002af",
+    "url": "/DreamReader/assets/js/app.afaf9a4a.js"
+  },
+  {
+    "revision": "95db0d3babf9be977ff1",
+    "url": "/DreamReader/assets/js/chunk-vendors.cf702a62.js"
+  },
+  {
+    "revision": "e752e0b26c34fc6973392f5d704fc2f2",
+    "url": "/DreamReader/index.html"
+  },
+  {
+    "revision": "723ba57fbc58c8c71423085038d2f3e9",
+    "url": "/DreamReader/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DreamReader/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DreamReader/precache-manifest.3e6f064debb35439004aa3e9b3729b11.js"
+  "/DreamReader/precache-manifest.06208b2d1456a38212cc1823cae1b8ba.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dreamreader"});
