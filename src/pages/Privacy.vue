4089e0c5eadc6ff98ccae7e5c3d52aa9210a7c13
SolAZDev <SolAZDev@gmail.com>
2021-03-14 02:50:38 -0400

    Edited Privacy Policy

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -4,18 +4,15 @@ q-page(padding)
   .q-ma-md
     q-list
       q-item
-        q-item-label #[b Your] dreams, #[b Your] journal, #[b Your] data. 
+        q-item-label #[b Your] dreams, #[b Your] journal, #[b #[u Your]] data. 
       q-item
-        q-item-label This app does #[b NOT] require internet
+        q-item-label This app does #[b NOT] track your usage as it does #[b NOT] require internet.
       q-item
         q-item-label We do #[b NOT] keep nor use your data. It stays on your device.
-      q-item
-        q-item-label We do #[b NOT] track your usage.
-
     
 </template>
 <script>
 export default {
   // name: 'PageName',
-}
+};
 </script>
